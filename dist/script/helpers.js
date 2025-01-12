b6f461674f89002bb5b2ef83585012135ae77378
Ne3M <ne3m.igor@gmail.com>
2025-01-12 15:14:56 +0100

    add inactive

--- dist/script/helpers.js
+++ dist/script/helpers.js
@@ -128,6 +128,7 @@ const generateTable = async (data, shouldSave=false) => {
     let totalDieselDeployed = 0;
     let totalUndeployed = 0;
     let totalLocked = 0;
+    let totalInactive = 0;
     data.result.members
       .sort((a, b) => {
         if (a.summary_power !== b.summary_power) {
@@ -146,6 +147,7 @@ const generateTable = async (data, shouldSave=false) => {
 
       totalUndeployed += (m.remaining_power > m.summary_power)? m.remaining_power - m.summary_power : 0;
       totalDeployed += m.summary_power;
+      totalInactive += (isActive) ? 0 : m.remaining_power;
       totalDieselDeployed += m.spent_elixir;
       totalLocked += isLocked ? m.remaining_power : 0;
 
@@ -174,6 +176,19 @@ const generateTable = async (data, shouldSave=false) => {
     <tr>
       <td class='player'>LOCKED TROOPS</td>
       <td colspan="3">${totalLocked.toLocaleString('es-ES')}</td>
+    </tr>
+    <tr>
+      <td class='player'>
+      INACTIVE TROOPS
+      <span title="This troops are not deducted from undeployed troops">
+          <svg viewBox="0 0 24 24" width="20px" height="20px" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 17.75C12.4142 17.75 12.75 17.4142 12.75 17V11C12.75 10.5858 12.4142 10.25 12 10.25C11.5858 10.25 11.25 10.5858 11.25 11V17C11.25 17.4142 11.5858 17.75 12 17.75Z" fill="#1C274C"></path> <path d="M12 7C12.5523 7 13 7.44772 13 8C13 8.55228 12.5523 9 12 9C11.4477 9 11 8.55228 11 8C11 7.44772 11.4477 7 12 7Z" fill="#1C274C"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M1.25 12C1.25 6.06294 6.06294 1.25 12 1.25C17.9371 1.25 22.75 6.06294 22.75 12C22.75 17.9371 17.9371 22.75 12 22.75C6.06294 22.75 1.25 17.9371 1.25 12ZM12 2.75C6.89137 2.75 2.75 6.89137 2.75 12C2.75 17.1086 6.89137 21.25 12 21.25C17.1086 21.25 21.25 17.1086 21.25 12C21.25 6.89137 17.1086 2.75 12 2.75Z" fill="#1C274C"></path>
+          </svg>
+        </span>
+      </td>
+      <td colspan="3">
+        ${totalInactive.toLocaleString('es-ES')} 
+        </td>
     </tr>`
     html += "</table>"
     html += "</table>"
