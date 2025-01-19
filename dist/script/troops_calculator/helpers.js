bc50cdff79c3043183f76b4e8b0bf702783cb45f
Ne3M <ne3m.igor@gmail.com>
2025-01-19 17:02:26 +0100

    Troops : better highlight + diff power

--- dist/script/troops_calculator/helpers.js
+++ dist/script/troops_calculator/helpers.js
@@ -13,15 +13,20 @@ const updateUnitCostLine = (currentUnit, recalculate = false) => {
     const cubeCostResult = currentUnit.querySelector('.total_cube_cost')
     const currentPowerResult = currentUnit.querySelector('.current_power')
     const newPowerResult = currentUnit.querySelector('.new_power')
+    const newPowerDiff = currentUnit.querySelector('.new_power_diff')
     const mercBonus = (currentUnit.querySelector('.merc_level_selector').value / 100) + 1
 
     const coreCost = troopsData[desiredLevel-1].totalCoreCost - troopsData[currentLevel-1].totalCoreCost
     const cubeCost = troopsData[desiredLevel-1].totalCubeCost - troopsData[currentLevel-1].totalCubeCost
+    const currentPower = Math.floor(troopsData[currentLevel-1].powerBase * rarityMultiplier[rarity] * mercBonus)
+    const nextPower = Math.floor(troopsData[desiredLevel-1].powerBase * rarityMultiplier[rarity] * mercBonus)
     
     coreCostResult.innerHTML = (coreCost > 0) ? coreCost : "0";
     cubeCostResult.innerHTML = (cubeCost > 0) ? cubeCost : "0";
-    currentPowerResult.innerHTML = Math.floor(troopsData[currentLevel-1].powerBase * rarityMultiplier[rarity] * mercBonus)
-    newPowerResult.innerHTML = Math.floor(troopsData[desiredLevel-1].powerBase * rarityMultiplier[rarity] * mercBonus)
+    currentPowerResult.innerHTML = currentPower
+    newPowerResult.innerHTML = nextPower
+    newPowerDiff.innerHTML = (nextPower - currentPower > 0) ? `(+${nextPower - currentPower})` : '';
+    
 
     if (recalculate) updateTotalWarpower()
 }
@@ -33,7 +38,7 @@ const updateTotalWarpower = () => {
     let mechCoreCost = 0;
     let airCoreCost = 0;
     let cubeCost = 0;
-    let mostCubeEffectiveUnit;
+    let mostCubeEffectiveUnits = [];
     let mostCubeEffectiveCost = 9999;
 
     for(let unit of units){
@@ -46,16 +51,22 @@ const updateTotalWarpower = () => {
         cubeCost += parseInt(unit.querySelector('.total_cube_cost').innerHTML);
         
         const cubePerPower = getCubePerPower(unit)
-        if( cubePerPower < mostCubeEffectiveCost) {
-            mostCubeEffectiveUnit = unit;
+        if( cubePerPower === mostCubeEffectiveCost ) {
+            mostCubeEffectiveUnits.push(unit)
+            mostCubeEffectiveCost = cubePerPower;
+        } else if ( cubePerPower < mostCubeEffectiveCost ) {
+            mostCubeEffectiveUnits = [unit];
             mostCubeEffectiveCost = cubePerPower;
         }
     }
     // console.log({mostCubeEffectiveUnit, mostCubeEffectiveCost})
-    mostCubeEffectiveUnit.classList.add('most_effective')
+    for( let mostCubeEffectiveUnit of mostCubeEffectiveUnits) {
+        mostCubeEffectiveUnit.classList.add('most_effective')
+    }
     
     document.querySelector('.warpower .current_power').innerHTML = currentWarPower;
     document.querySelector('.warpower .new_power').innerHTML = futureWarPower;
+    document.querySelector('.warpower .new_power_diff').innerHTML = (futureWarPower - currentWarPower > 0) ? `(+${futureWarPower - currentWarPower})` : '';;
     document.querySelector('.warpower .upgrade_costs .total_tankcore_cost').innerHTML = tankCoreCost;
     document.querySelector('.warpower .upgrade_costs .total_mechcore_cost').innerHTML = mechCoreCost;
     document.querySelector('.warpower .upgrade_costs .total_aircore_cost').innerHTML = airCoreCost;
@@ -63,8 +74,12 @@ const updateTotalWarpower = () => {
 }
 
 const getCubePerPower = (unit) => {
+    
     const rarity = unit.dataset.rarity
+    const currentLevel = parseInt(unit.querySelector('.unit_level input').value)
     const desiredLevel = parseInt(unit.querySelector('.unit_level input:last-of-type').value)
+
+    if(currentLevel >= desiredLevel) return 9999
 
     const plus10LvlCost = troopsData[desiredLevel+9].totalCubeCost - troopsData[desiredLevel-1].totalCubeCost
     const plus10LvlPower = Math.floor(troopsData[desiredLevel+9].powerBase * rarityMultiplier[rarity]) - Math.floor(troopsData[desiredLevel-1].powerBase * rarityMultiplier[rarity])
