9d7250675a8e230f4862d69d09416677f66ecfa5
Ne3M <ne3m.igor@gmail.com>
2026-08-12 16:07:33 +0200

    feat(autoLeaderboard): setRetries to 2 on CI

--- playwright.config.js
+++ playwright.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   name: 'chromium',
   use: {
     // channel: 'chromium', // full browser even in headless
+    retries: process.env.CI ? 2 : 0,
     permissions: ['storage-access'],
     viewport: { width: 1280, height: 720 },
     // headless: false,
