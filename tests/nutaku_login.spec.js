bd6b68d3ec2b2fc1cf53e0a5de5cb24f4887653f
Ne3M <ne3m.igor@gmail.com>
2026-08-12 15:22:22 +0200

    feat(autoLeaderboard): add screenshot for last leaderboard

--- tests/nutaku_login.spec.js
+++ tests/nutaku_login.spec.js
@@ -2,18 +2,66 @@ import { test, expect } from '@playwright/test';
 import fs from 'node:fs/promises';
 import path from 'node:path';
 
+const currentTime = Date.now();
+const outputFolder = path.resolve(process.cwd(), 'dist', 'data');
+const outputPath = path.join(outputFolder, 'credentials.json');
+
+// await fs.mkdir(outputFolder, { recursive: true });
+
+
+const credentials = {
+  version: null,
+  deviceId: null,
+  playerId: null,
+  sessionId: null,
+  stillValid: null,
+  updateTime: null
+}
+
 test.use({ userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36' });
 
-const result = {
-  version: null,
-  device: null,
-  playerId: null,
-  sessionId: null
-}
+test('Check credentials validity', async ({page}) => {
+
+  const oldCredentials = JSON.parse(await fs.readFile(outputPath, 'utf8').catch(() => null));
+  console.log({oldCredentials})
+  
+  const url = 'https://ga.chickgoddess.com/gs_api/guilds/get_members';
+  const options = {
+    method: 'POST',
+    headers: {
+      'pnk-device-id': oldCredentials.deviceId,
+      'pnk-env': 'WebGLPlayer',
+      'pnk-player-id': oldCredentials.playerId,
+      'pnk-platform': 'NUTAKU',
+      'pnk-request-client-start-time': new Date().toISOString(),
+      'pnk-session-id': oldCredentials.sessionId,
+      'pnk-version': oldCredentials.version,
+      'content-type': 'application/json'
+    },
+    body: '{"guild_id":"4380","members":false,"recruits":false,"requests":false,"invites":false}'
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    console.log({data, options});
+    credentials.stillValid = !!data.result.guild
+    credentials.deviceId = oldCredentials.deviceId
+    credentials.playerId = oldCredentials.playerId
+    credentials.sessionId = oldCredentials.sessionId
+    credentials.version = oldCredentials.version
+    credentials.updateTime = oldCredentials.updateTime
+  } catch (error) {
+    console.error(error);
+    credentials.stillValid = false
+  }
+  console.log("stillValid :", credentials.stillValid)
+})
 
 test('Get Lust Goddess Credentials',  async ({page}) => {
 
-    
+    test.skip(credentials.stillValid, 'La fonctionnalité n\'est pas active sur cet environnement.');
+
     // Go to game page
     await page.goto('https://www.nutaku.net/fr/', {waitUntil: "domcontentloaded"});
 
@@ -50,8 +98,8 @@ test('Get Lust Goddess Credentials',  async ({page}) => {
 
 
     const req = await page.waitForRequest(r => r.url().includes('/gs_api/profile/nop'), { timeout: 120000 });
-    result.device = await req.headerValue("pnk-device-id")
-    result.version = await req.headerValue("pnk-version")
+    credentials.deviceId = await req.headerValue("pnk-device-id")
+    credentials.version = await req.headerValue("pnk-version")
     expect(req).toBeTruthy();
 
     await page.waitForTimeout(5000); // wait for 3 seconds
@@ -60,22 +108,52 @@ test('Get Lust Goddess Credentials',  async ({page}) => {
     console.log('Loading the game...');
     const req2 = await page.waitForResponse(r => r.url().includes('get_parent_profile_id'), { timeout: 120000 });
     const body = await req2.json()
-    result.sessionId = body.result.session_id
-    result.playerId = body.result.profile_id
-    result.updateTime = new Date().toISOString().slice(0, 16); 
+    credentials.sessionId = body.result.session_id
+    credentials.playerId = body.result.profile_id
+    credentials.updateTime = new Date().toISOString().slice(0, 16); 
 
-    console.log(result)
+    console.log(credentials)
 
     const outputFolder = path.resolve(process.cwd(), 'dist', 'data');
     const outputPath = path.join(outputFolder, 'credentials.json');
 
     // await fs.mkdir(outputFolder, { recursive: true });
-    await fs.writeFile(outputPath, JSON.stringify(result, null, 2), 'utf8');
+    await fs.writeFile(outputPath, JSON.stringify(credentials, null, 2), 'utf8');
 
     // expect result to have no null value
-    expect(result.version).not.toBeNull();
-    expect(result.device).not.toBeNull();
-    expect(result.playerId).not.toBeNull();
-    expect(result.sessionId).not.toBeNull();  
+    expect(credentials.version).not.toBeNull();
+    expect(credentials.deviceId).not.toBeNull();
+    expect(credentials.playerId).not.toBeNull();
+    expect(credentials.sessionId).not.toBeNull();  
   
 });
+
+test('Screenshot Leaderboard', async ({page}) => {
+
+  await page.goto(`https://fumicon-war-tools.netlify.app/leaderboard.html?PNK-Player-ID=${credentials.playerId}&PNK-Session-Id=${credentials.sessionId}&PNK-Version=${credentials.version}`)
+  
+  // Prépare l'attente de l'événement dans le navigateur
+  const waitForCustomEvent = page.waitForFunction(() => {
+    return new Promise((resolve) => {
+      window.addEventListener('leaderboardUpdated', () => resolve(true), { once: true });
+    });
+  });
+
+  // page.on('request', async data => {
+  //     console.log(await data.postData(), await data.url())
+  // });
+  await page.addStyleTag({
+    content: `
+      .result tr:nth-of-type(n+32) {
+        display: none;
+      }
+    `
+  });
+
+  await page.locator('#updateLeaderBoard').first().click();
+  await waitForCustomEvent;
+  console.log('L\'événement JS a bien été reçu !');
+
+  await page.locator('.result').screenshot({ path: 'dist/data/latest_leaderboard.png' });
+
+})
